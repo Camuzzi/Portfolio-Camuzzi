6e67820434e845873d40f276eb83bb38338d1a7c
Camuzzi <camuzziagustin@gmail.com>
2023-10-26 15:40:27 -0300

    Font added

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,7 +2,17 @@
 const Home = () => {
     return(
         <div>
-            <h1>HOME</h1>
+            <h1><b>Hello!<br /> I'm Agustin Camuzzi<br /> Full Stack Developer</b> </h1>
+            <h2><br /> Welcome to my portfolio</h2>
+            <br />
+            <button >
+                Contact Me
+            </button>
+            <br />
+            <br />
+            <button >
+                Projects
+            </button>
         </div>
     );
 }
