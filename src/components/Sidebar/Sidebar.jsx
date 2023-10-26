4ea249d7c3285c5caf66900efc2f66ff1228c4fe
Camuzzi <camuzziagustin@gmail.com>
2023-10-26 15:00:00 -0300

    sidebar created and components modularizated

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,47 +1,54 @@
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
+import homeLogo from "../../assets/home.svg";
+import aboutLogo from "../../assets/about.svg";
+import skillsLogo from "../../assets/skills.svg";
+import projectsLogo from "../../assets/projects.svg";
+import contactLogo from "../../assets/contact.svg";
+import linkedInLogo from "../../assets/linkedIn.svg";
+import gitLogo from "../../assets/github.svg";
 
 const Sidebar = () => {
     return(
         <div className="sidebar">
             <div className="sidebar-header">
-                <h3>Agustin Camuzzi</h3>
+                <h3>Agustin <br /> Camuzzi</h3>
             </div>
             <ul className="sidebar-icons">
                 <li>
                     <Link to="/home">
-                         Home
+                         <img src={homeLogo} alt="Home Logo" title="Home" className="icon"/>
                     </Link>
                 </li>
                 <li>
                     <Link to="/about">
-                         About
+                    <img src={aboutLogo} alt="About Logo" title="About" className="icon"/>
                     </Link>
                 </li>
                 <li>
                     <Link to="/skills">
-                         Skills
+                    <img src={skillsLogo} alt="Skills Logo" title="Skills" className="icon"/>
                     </Link>
                 </li>
                 <li>
                     <Link to="/projects">
-                         Projects
+                    <img src={projectsLogo} alt="Projects Logo" title="Projects" className="icon"/>
                     </Link>
                 </li>
                 <li>
                     <Link to="/contact">
-                         Contact me
+                    <img src={contactLogo} alt="Contact Logo" title="Contact me" className="icon"/>
                     </Link>
                 </li>
+                <a href="https://www.linkedin.com/in/agustin-camuzzi-3a9b81272/" target="_blank">
+                <img src={linkedInLogo} alt="Linked In Logo" title="LinkedIn" className="icon-social"/>
+                </a>
+                <a href="'https://github.com/Camuzzi'" target="_blank">
+                <img src={gitLogo} alt="GitHub Logo" title="GitHub" className="icon-social"/>
+                </a>
             </ul>
-            <div className="sidebar-icons">
-                <a href="" target="">
-                    Linked In
-                </a>
-                <a href="" target="">
-                    GitHub
-                </a>
-            </div>
+            
+            
         </div>
     );
 }
